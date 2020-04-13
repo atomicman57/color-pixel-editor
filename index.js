9f6176063f27452229ecff1ba37f0fe3026ffb95
Blessing Philips <blessingphilips@Blessings-MacBook-Pro.local>
2020-04-13 04:22:44 +0100

    feat: implement color picker

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-// import { a } from "./moduleA";
+import APP_CONSTANTS from './utils/app_constants'
+import Picker from './modules/Picker';
 
-// console.log(a);
+
+window.addEventListener('load', () => {
+    const pickerElement = document.getElementById(APP_CONSTANTS.SLIDER_SELECTOR);
+    const secondpickerElement = document.getElementById(APP_CONSTANTS.SLIDER_2_SELECTOR);
+    const picker = new Picker(pickerElement, secondpickerElement)
+
+    pickerElement.addEventListener("input", (e) => {
+        secondpickerElement.value = e.target.value
+        picker.onColorChange(e.target.value);
+    });
+
+    secondpickerElement.addEventListener("input", (e) => {
+        pickerElement.value = e.target.value
+        picker.onColorChange(e.target.value);
+    });
+
+})
+
--- modules/Picker.js
+++ modules/Picker.js
@@ -1,4 +1,30 @@
 class Picker {
+    constructor(pickerElement, secondpickerElement) {
+        this.state = {
+            hsl: {
+                h: 180,
+                s: 100,
+                l: 50
+            },
+            rgb: {
+                r: '',
+                g: '',
+                b: ''
+            }
+        }
+
+        this.pickerElement = pickerElement;
+        this.secondpickerElement = secondpickerElement;
+        this.onColorChange(this.state['hsl'].h)
+    }
+
+    convertValueToRgb() {
+
+    }
+
+    onColorChange(value) {
+        console.log('value', value)
+    }
 }
 
 export default Picker
