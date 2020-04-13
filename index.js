786155746039fa36d7f364732269b86a7c41c913
Blessing Philips <blessingphilips@Blessings-MacBook-Pro.local>
2020-04-13 22:08:44 +0100

    feat: implement color slider and color container

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ import Picker from './modules/Picker';
 window.addEventListener('load', () => {
     const pickerElement = document.getElementById(APP_CONSTANTS.SLIDER_SELECTOR);
     const secondpickerElement = document.getElementById(APP_CONSTANTS.SLIDER_2_SELECTOR);
-    const picker = new Picker(pickerElement, secondpickerElement)
+    const colorContainer = document.getElementById(APP_CONSTANTS.COLOR_CONTAINER_SELECTOR);
+    const picker = new Picker(colorContainer)
 
     pickerElement.addEventListener("input", (e) => {
         secondpickerElement.value = e.target.value
--- modules/Picker.js
+++ modules/Picker.js
@@ -1,29 +1,55 @@
+import { convertHSLToRGB, convertRGBToHex } from '../utils/converter'
+
 class Picker {
-    constructor(pickerElement, secondpickerElement) {
+    constructor(colorContainer) {
         this.state = {
-            hsl: {
-                h: 180,
-                s: 100,
-                l: 50
-            },
-            rgb: {
-                r: '',
-                g: '',
-                b: ''
-            }
+            inputValue: 0,
+            h: 180,
+            s: 100,
+            l: 50,
+            r: 0,
+            g: 255,
+            b: 255,
+            hex: ''
         }
 
-        this.pickerElement = pickerElement;
-        this.secondpickerElement = secondpickerElement;
-        this.onColorChange(this.state['hsl'].h)
+        this.colorContainer = colorContainer;
+        this.onColorChange(this.state['h'])
     }
 
-    convertValueToRgb() {
+    getState(variable) {
+        return this.state[variable];
+    }
 
+    setState(variable, value) {
+        this.state[variable] = value;
+    }
+
+    convertValueToRgb(value) {
+        const rgb =  convertHSLToRGB(value,  this.getState('s'),this.getState('l'))
+        this.setState('r', rgb.r)
+        this.setState('g', rgb.g)
+        this.setState('g', rgb.b)
+        return "rgb(" + rgb.r + "," + rgb.g + "," + rgb.b + ")";
     }
 
     onColorChange(value) {
-        console.log('value', value)
+        const rgb = this.convertValueToRgb(value)
+        this.updateSliderThumbColor(rgb)
+        this.colorContainer.style.backgroundColor = rgb;
+        const hex = convertRGBToHex(this.getState('r'), this.getState('g'), this.getState('b'))
+        this.setState('hex', hex);
+        this.colorContainer.innerHTML = hex;
+    }
+
+    updateSliderThumbColor(color) {
+        const styles = `.slider::-webkit-slider-thumb, .slider-2::-webkit-slider-thumb { background-color: ${color}; }`
+        this.updateStyle(styles)
+    }
+
+    updateStyle(styles) {
+        let style = document.querySelector('[name="updateStyle"]')
+        style.innerHTML = styles
     }
 }
 
